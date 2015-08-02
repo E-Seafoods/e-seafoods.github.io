7e1df9d270d042a10ab351ef8d4fa5f2e98cdeb3
Bryan Chug <bryan.chug@gmail.com>
2015-08-02 20:49:00 +0800

    Updates

--- _scripts/src/mida-scraper.groovy
+++ _scripts/src/mida-scraper.groovy
@@ -1,2 +1,29 @@
-String url = "http://www.midafood.com/products/crab-softshell"
-println url
+#!/usr/bin/env groovy
+
+@Grapes( @Grab('org.ccil.cowan.tagsoup:tagsoup:1.2') )
+
+import org.ccil.cowan.tagsoup.Parser;
+
+String ENCODING = "UTF-8"
+
+def cli = new CliBuilder(usage: 'mida-scraper [url]')
+
+def options = cli.parse(args)
+
+String url
+if (options.arguments()) {
+    url = options.arguments()[0]
+}else{
+    cli.usage()
+    return
+}
+
+println "Scraping ${url}..."
+
+def parser = new XmlSlurper(new Parser() )
+
+new URL(url).withReader (ENCODING) { reader ->
+    def document = parser.parse(reader)
+//    Extracting information
+}
+
