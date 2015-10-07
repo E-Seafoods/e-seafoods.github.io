5200aaa2d045c2eb11f0f6a62372e3dbe71823e7
rensramos <rensramos17@gmail.com>
2015-10-07 18:25:54 +0800

    Add function

--- js/lightImageSlider.js
+++ js/lightImageSlider.js
@@ -1,11 +1,36 @@
 var esf = esf || {};
 esf.imageSlider = function() {
     esf.productImageSlider();
+    esf.relatedProductSlider();
 };
 
 esf.productImageSlider = function(){
-    var $autoPlaySlider = esf.autoPlaySlider;
-    autoPlaySlider = $('.product-img-slider').lightSlider({
+    //var $autoPlaySlider = esf.autoPlaySlider;
+        $('#product-img-slider').lightSlider({
+            gallery:true,
+            item:1,
+            loop:true,
+            thumbItem:4,
+            slideMargin:0,
+            mode: 'fade',
+            enableDrag: false,
+            controls: false,
+            speed: 1000,
+            pause: 6000,
+            auto: true,
+            currentPagerPosition:'left',
+            onSliderLoad: function(el) {
+                el.lightGallery({
+                    selector: '#product-img-slider .lslide'
+                });
+            }
+        });
+
+    };
+
+esf.relatedProductSlider = function(){
+    //var $relatedAutoPlaySlider = esf.autoPlaySlider;
+    autoPlaySlider = $('#related-product-slider').lightSlider({
         item: 4,
         controls: true,
         speed: 2000,
